62af25c94cef3282bdc823dc0c72944887061aaa
mhmd nofal <mhd.nofal@sheenvalue.com>
2023-06-03 19:26:42 +0300

    chore: initial commit

---
+++ android/src/main/java/com/backgroundrunner/BackgroundRunnerModule.java
@@ -0,0 +1,32 @@
+package com.backgroundrunner;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = BackgroundRunnerModule.NAME)
+public class BackgroundRunnerModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "BackgroundRunner";
+
+  public BackgroundRunnerModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void multiply(double a, double b, Promise promise) {
+    promise.resolve(a * b);
+  }
+}
